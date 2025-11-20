3510338711488b2d9f0ff7d31e0d19f3538bcc4d
pepe1224 <josemauriciogonzalez11@gmail.com>
2025-11-20 14:22:17 +0100

    ñhg

---
+++ Comprobaciones.py
@@ -0,0 +1,13 @@
+# """ def contar_hasta():
+#     for i in range(1, 6):
+#         print(i)
+
+# contar_hasta() """
+#Comprobación echa de ap1
+def doblar(x):
+    return x * 2 
+doblar(5)
+mostrar_doble()
+def mostrar_doble():
+    valor = 7
+    print(doblar(valor))
