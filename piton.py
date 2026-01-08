bace62e0d5c2ceceb19fa6757990dd78891f3ca8
pepe1224 <josemauriciogonzalez11@gmail.com>
2026-01-08 12:49:51 +0100

    sdfg

--- piton.py
+++ piton.py
@@ -1,12 +1,16 @@
 #cadena = 'cadenas'
 #print(cadena[0:4])
-a = 'hola'
-b = 9
-print(a + str(b))
+# a = 'hola'
+# b = 9
+# print(a + str(b))
 
-texto = 'sosad; cola; espuma'
-cols = texto.split(',')
-for col in cols:
-    print(col)
+# texto = 'sosad; cola; espuma'
+# cols = texto.split(',')
+# for col in cols:
+#     print(col)
 
 
+color = [1, 3, 2]
+#color.remove('azul')
+color.pop(1)
+print(color)
