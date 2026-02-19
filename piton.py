c57801ac410142f558c53d46d68aa75e3431abca
pepe1224 <josemauriciogonzalez11@gmail.com>
2026-02-19 07:49:03 +0100

    adsadwa

--- piton.py
+++ piton.py
@@ -20,18 +20,40 @@
 # print(a + " " + b) 
 
 
-class Hucha:
-    def __init__(self):
-        self.__dinero = 0
+# class Hucha:
+#     def __init__(self):
+#         self.__dinero = 0
 
-    def meter_dinero(self, cantidad):
-        if cantidad > 0:
-            self.__dinero += cantidad
+#     def meter_dinero(self, cantidad):
+#         if cantidad > 0:
+#             self.__dinero += cantidad
     
-    def ver_dinero(self):
-        return self.__dinero
+#     def ver_dinero(self):
+#         return self.__dinero
     
-hucha = Hucha
-hucha.meter_dinero(10)
-print(hucha.ver_dinero())
-assert hucha.ver_dinero()
+# hucha = Hucha
+# hucha.meter_dinero(10)
+# print(hucha.ver_dinero())
+# assert hucha.ver_dinero()
+
+
+
+
+
+# var = input("hola, tu nombre es: ")
+# opc = input(var + ", escoge una opcion: ")
+# print(opc)
+class Pizza():
+   def __init__(self, pepino, tomate, cebolla):
+      self.pepino = pepino
+      self.tomate = tomate
+      self.cebolla = cebolla
+    
+    
+    def fetuchini():
+        var = int(input("escoge un numero del 0 al 3: "))
+        while True:
+          if var == 0:
+             break
+          elif var == 1:
+             pass
--- tema_5/pizzeria_base_alumno/pizza.py
+++ tema_5/pizzeria_base_alumno/pizza.py
@@ -8,30 +8,46 @@ class Pizza:
 
     def add_ingrediente(self, ingrediente):
         self.__ingredientes.append(ingrediente)
+            
 
     def imprimir_ascii(self):
+        aux_ingredientes = []
+
+        for ingredientes in self.__ingredientes:
+            if ingredientes.get_simbolo() not in aux_ingredientes:
+                aux_ingredientes.append(ingredientes.get_simbolo())
+
+        lista_ingredientes = []
+        if(len(aux_ingredientes) == 1):
+            lista_ingredientes = [aux_ingredientes[0], aux_ingredientes[0], aux_ingredientes[0]]
+        if(len(aux_ingredientes) == 2):
+            lista_ingredientes = [aux_ingredientes[1], aux_ingredientes[1], aux_ingredientes[0]]
+        if (len(aux_ingredientes) == 3):
+            lista_ingredientes = [aux_ingredientes[2], aux_ingredientes[2], aux_ingredientes[0]]
+
+            pipsa = f"""
+                  🍞🍞🍞🍞🍞🍞🍞🍞🍞🍞
+                🍞🍅🍅🍅🍅🍅🍅🍅🍅🍅🍅🍞
+               🍞🍅{lista_ingredientes[0]}🍅🍅{self.__ingredientes[0].get_simbolo()}🍅🍅🍅{self.__ingredientes[1].get_simbolo()}🍅🍅{self.__ingredientes[2].get_simbolo()}🍅🍞
+              🍞🍅                        🍅🍞
+             🍞🍅                          🍅🍞
+            🍞🍅                           🍅🍞
+            🍞🍅                           🍅🍞
+            🍞🍅                           🍅🍞
+            🍞🍅                           🍅🍞
+            🍞🍅                           🍅🍞
+            🍞🍅                           🍅🍞
+            🍞🍅                           🍅🍞
+            🍞🍅                           🍅🍞
+            🍞🍅                           🍅🍞
+            🍞🍅                           🍅🍞
+            🍞🍅                           🍅🍞
+            🍞🍅                           🍅🍞
+             🍞🍅                         🍅🍞
+              🍞🍅                       🍅🍞
+               🍞🍅                     🍅🍞
+                 🍞🍅🍅🍅🍅🍅🍅🍅🍅🍅🍞
+                    🍞🍞🍞🍞🍞🍞🍞🍞🍞
         """
-              🍞🍞🍞🍞🍞🍞🍞🍞🍞🍞
-            🍞🍅🍅🍅🍅🍅🍅🍅🍅🍅🍅🍞
-           🍞🍅{                    }🍅🍞
-          🍞🍅{                      }🍅🍞
-         🍞🍅{                        }🍅🍞
-        🍞🍅{                         }🍅🍞
-        🍞🍅{                         }🍅🍞
-        🍞🍅{                         }🍅🍞
-        🍞🍅{                         }🍅🍞
-        🍞🍅{                         }🍅🍞
-        🍞🍅{                         }🍅🍞
-        🍞🍅{                         }🍅🍞
-        🍞🍅{                         }🍅🍞
-        🍞🍅{                         }🍅🍞
-        🍞🍅{                         }🍅🍞
-        🍞🍅{                         }🍅🍞
-        🍞🍅{                         }🍅🍞
-         🍞🍅{                       }🍅🍞
-          🍞🍅{                     }🍅🍞
-           🍞🍅{                   }🍅🍞
-             🍞🍅🍅🍅🍅🍅🍅🍅🍅🍅🍞
-                🍞🍞🍞🍞🍞🍞🍞🍞🍞
-        """
-        return
+            print(pipsa)
+            return pipsa
